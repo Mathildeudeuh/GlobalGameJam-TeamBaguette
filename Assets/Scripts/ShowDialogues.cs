d6fc1bdf958a289797366cf74187f9265641908a
Mathildeuh <m.degris@etudiant.ecolemelies.fr>
2023-01-31 11:46:05 +0100

    Le texte affiche la première ligne de dialogue avec un bouton

---
+++ Assets/Scripts/ShowDialogues.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ShowDialogues : MonoBehaviour
+{
+    public DialoguesSO dialoguesSO;
+    public bool isSpeaking;
+    public TextMeshProUGUI texte;
+    public int dialogueLine;
+
+
+    private void Start()
+    {
+        isSpeaking = false;
+    }
+
+
+    public void StartDialogue()
+    {
+        texte.text = dialoguesSO.dialogues[dialogueLine].ToString();
+    }
+
+    /*public void NextLine()
+    {
+        if (isSpeaking == true && dialogueLine != dialoguesSO.dialogues.Length - 1)
+        {
+            dialogueLine++;
+            texte.text = dialoguesSO.dialogues[dialogueLine].ToString();
+        }
+        else if(isSpeaking == true && dialogueLine == dialoguesSO.dialogues.Length -1)
+        {
+            isSpeaking = false;
+        }
+    }*/
+}
