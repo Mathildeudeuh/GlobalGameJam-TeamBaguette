2b14865eac61ce4f1581482cfb457c08b3fedec7
Maria <mf.berguerand@etudiant.ecolemelies.fr>
2023-02-01 17:05:30 +0100

    SpotRelous

---
+++ Assets/Scripts/FollowPlayer/FollowWhenCollide03.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class FollowWhenCollide03 : MonoBehaviour
+{
+    
+    public GameObject Target;
+    public float Distance;
+
+    void Update()    //update
+    {
+        
+        Distance = Vector2.Distance(transform.position, Target.transform.position);
+
+        if (Distance < 3)
+        {
+
+            if (Distance >= 6)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, Target.transform.position, 70 * Time.deltaTime);
+
+            }
+        }
+        
+    }
+
+}
