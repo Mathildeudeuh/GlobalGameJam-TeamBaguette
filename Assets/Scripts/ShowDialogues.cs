9c624cfa2a8817e23b4bf9f909bb021f58bd72e6
Mathilde <m.degris@etudiant.ecolemelies.fr>
2023-02-01 23:14:53 +0100

    Change code ShowDialogue
    ça s'affiche au start

--- Assets/Scripts/ShowDialogues.cs
+++ Assets/Scripts/ShowDialogues.cs
@@ -8,23 +8,54 @@ public class ShowDialogues : MonoBehaviour
     public DialoguesSO dialoguesSO;
     public bool isSpeaking;
     public TextMeshProUGUI texte;
+    public float textSpeed;
     public int dialogueLine;
+    public GameObject dialogueBox;
+    
 
 
     private void Start()
     {
-        isSpeaking = false;
+        texte.text = string.Empty;
+        StartDialogue();
+        //isSpeaking = false;
     }
 
 
     public void StartDialogue()
     {
-        texte.text = dialoguesSO.dialogues[dialogueLine].ToString();
+        dialogueLine = 0;
+        StartCoroutine(TypeLine());
+        //texte.text = dialoguesSO.dialogues[dialogueLine].ToString();
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in dialoguesSO.dialogues[dialogueLine].ToCharArray())
+        {
+            texte.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
     }
 
     public void NextLine()
-    {
-        if (isSpeaking == true && dialogueLine != dialoguesSO.dialogues.Length - 1)
+    {     
+        if (dialogueLine < dialoguesSO.dialogues.Length - 1)
+        {
+            dialogueLine++;
+            dialogueLine = 0;
+            StartCoroutine(TypeLine());
+        }
+
+        else
+        {
+            dialogueBox.SetActive(false);
+        }
+
+
+
+
+        /*if (isSpeaking == true && dialogueLine != dialoguesSO.dialogues.Length - 1)
         {
             dialogueLine++;
             texte.text = dialoguesSO.dialogues[dialogueLine].ToString();
@@ -33,6 +64,9 @@ public class ShowDialogues : MonoBehaviour
         else if (isSpeaking == true && dialogueLine == dialoguesSO.dialogues.Length -1)
         {
             isSpeaking = false;
-        }
+        }*/
+
+
+
     }
 }
